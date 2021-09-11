1fcc7e002496d275143de6ca542209caa18be0d4
Alex <2640178@gmail.com>
2021-09-11 15:25:45 +0300

    Задание 2

---
+++ src/main/java/l2/LinkedListImpl.java
@@ -0,0 +1,154 @@
+package l2;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedListImpl<E> implements LinkedList<E> {
+
+    protected Node<E> firstElement;
+    protected int size;
+
+
+    public void insertFirst(E value) {
+        firstElement = new Node<>(value, firstElement);
+        size++;
+    }
+
+
+    public E removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        Node<E> removedNode = firstElement;
+        firstElement = removedNode.next;
+        removedNode.next = null;
+        size--;
+
+        return removedNode.item;
+    }
+
+    @Override
+    public boolean remove(E value) {
+        Node<E> current = firstElement;
+        Node<E> previous = null;
+
+        while (current != null) {
+            if (current.item.equals(value)) {
+                break;
+            }
+            previous = current;
+            current = current.next;
+        }
+
+        if (current == null) {
+            return false;
+        } else if(current == firstElement) {
+            removeFirst();
+            return true;
+        } else {
+            previous.next = current.next;
+        }
+
+        current.next = null;
+        size--;
+
+        return true;
+    }
+
+    public boolean contains(E value) {
+        Node<E> current = firstElement;
+        while (current != null) {
+            if (current.item.equals(value)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder("[");
+        Node<E> current = firstElement;
+        while (current != null) {
+            sb.append(current.item);
+            if (current.next != null) {
+                sb.append(" -> ");
+            }
+
+            current = current.next;
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public E getFirst() {
+        return getValue(firstElement);
+    }
+
+    protected E getValue(Node<E> eNode) {
+        return eNode != null ? eNode.item : null;
+    }
+
+    private class LinkedListIterator implements Iterator<E> {
+        private final LinkedListImpl<E> list;
+        private Node<E> current;
+        private Node<E> previous;
+
+        public LinkedListIterator() {
+            this.list = LinkedListImpl.this;
+            reset();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            E nextValue = current.item;
+            previous = current;
+            current = current.next;
+            return nextValue;
+        }
+
+        @Override
+        public void remove() {
+            if (previous == null) {
+                list.firstElement = current.next;
+                reset();
+            } else {
+                previous.next = current.next;
+                if (!hasNext()) {
+                    reset();
+                } else {
+                    current = current.next;
+                }
+            }
+        }
+
+        public void reset() {
+            current = list.firstElement;
+            previous = null;
+        }
+    }
+}
